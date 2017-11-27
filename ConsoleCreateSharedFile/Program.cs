787e79be6549d55843175c0c755b57d2274c38bc
ArmasterCZ <akrijfuadlaf@seznam.cz>
2017-11-27 18:14:36 +0100

    V0:00:08 - oprávnění, zvuk

--- ConsoleCreateSharedFile/Program.cs
+++ ConsoleCreateSharedFile/Program.cs
@@ -7,7 +7,7 @@ using System.Configuration;
 using System.IO;
 using System.Security.AccessControl;
 
-// V0:00:07
+// V0:00:08
 // program for create folders for offline files with share rights
 
 namespace ConsoleCreateSharedFile
@@ -21,9 +21,32 @@ namespace ConsoleCreateSharedFile
 
             //spuštění zvuku
             //System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
-            //player.Play();
-            //player.Stop();
+            
+            string pathWAV = @"D:\Users\jvaldauf\Documents\Visual Studio 2015\GITHUB\ConsoleCreateSharedFile\ConsoleCreateSharedFile\resources\houk.wav";
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(pathWAV);
+            player.Play();
+            Console.WriteLine("houkám");
+            Console.ReadLine();
+            player.Stop();
+            Console.WriteLine("nehoukám");
 
+
+            string path = "C:\\Nová_složka\\složka na test oprávnění";
+            fileNew(path);
+
+            Console.WriteLine(path);
+            Console.WriteLine("");
+            RemoveDirectorySecurity(path, @"Authenticated Users", FileSystemRights.ReadData, AccessControlType.Allow);
+            AddDirectorySecurity(path, @"sitel\Domain Admins", FileSystemRights.ReadData, AccessControlType.Allow);
+            AddDirectorySecurity(path, @"sitel\ftester", FileSystemRights.ReadData, AccessControlType.Allow);
+            ReadDirectoryRight(path);
+
+            Console.ReadLine();
+        }
+
+        // create paths for offlineFiles
+        public static void folderNames()
+        {
             //sestavení stringu
             //string test = String.Format("{0} necco neco {1}", "-1-", "-2-");
 
@@ -40,23 +63,15 @@ namespace ConsoleCreateSharedFile
             string fileShareName = department + "_" + PcName;
             //outside server
             string fileSharePath = "\\\\" + serverName + "\\" + fileShareName;
-            string fileSharePathFull = "\\\\" + serverName + "\\D$\\"+department+"\\backup_ntb\\" + fileShareName;
+            string fileSharePathFull = "\\\\" + serverName + "\\D$\\" + department + "\\backup_ntb\\" + fileShareName;
             //in server
             string fileServerPathFull = "D:\\" + department + "\\backup_ntb\\" + fileShareName;
 
             Console.WriteLine(fileSharePathFull);
             Console.WriteLine(fileServerPathFull);
-
-            string path = "C:\\složka";
-            fileNew(path);
-            FileSystemRights right = new FileSystemRights();
-            AccessControlType access = new AccessControlType();
-            
-            AddDirectorySecurity("složka", "jvaldaf", right, access);
-            Console.ReadLine();
         }
 
-        //read string from .config
+        // read string from .config
         static string readSetting(string key)
         {
             //string test1 = ConfigurationManager.AppSettings["Setting1"]; //one line simple load
@@ -101,10 +116,7 @@ namespace ConsoleCreateSharedFile
             }*/
         }
 
-        public static void removeDirectoryRight()
-        {
-
-        }
+        #region zabezpeceni
 
         // check // Adds an ACL entry on the specified directory for the specified account.
         public static void AddDirectorySecurity(string FileName, string Account, FileSystemRights Rights, AccessControlType ControlType)
@@ -146,6 +158,52 @@ namespace ConsoleCreateSharedFile
 
         }
 
+        // read Directory right
+        public static void ReadDirectoryRight(string FileName)
+        {
+            // Create a new DirectoryInfo object.
+            DirectoryInfo dInfo = new DirectoryInfo(FileName);
+
+            // Get a DirectorySecurity object that represents the 
+            // current security settings.
+            DirectorySecurity dSecurity = dInfo.GetAccessControl();
+
+            AuthorizationRuleCollection rules = dSecurity.GetAccessRules(true, false, typeof(System.Security.Principal.NTAccount));
+            foreach (FileSystemAccessRule rule in rules)
+            {
+                Console.WriteLine(rule.IdentityReference);
+                Console.WriteLine("     " + rule.AccessControlType);
+            }
+        }
+
+        #endregion zabezpeceni
+
+        public static void shareFolder(string fileName)
+        {
+            //I already use AccessControl ...
+            DirectoryInfo dInfo = new DirectoryInfo(fileName);
+            DirectorySecurity dSecurity = dInfo.GetAccessControl();
+            dSecurity.AddAccessRule(new FileSystemAccessRule("everyone", FileSystemRights.FullControl, InheritanceFlags.ObjectInherit | InheritanceFlags.ContainerInherit, PropagationFlags.InheritOnly, AccessControlType.Allow));
+            dInfo.SetAccessControl(dSecurity);
+
+            ////... but it allow me to set "Security permission" not "Shared permission".
+            ////To activate sharing in the directory I use WMI
+            //ManagementClass mc = new ManagementClass("win32_share");
+            //ManagementBaseObject inParams = mc.GetMethodParameters("Create");
+            //inParams("Description") = "My Shared Folder";
+            //inParams("Name") = "Shared Folder Name";
+            //inParams("Path") = "C:\\Folder1";
+            //inParams("Type") = ShareResourceType.DiskDrive;
+            //inParams("MaximumAllowed") = null;
+            //inParams("Password") = null;
+            //inParams("Access") = null; // Make Everyone has full control access.
+            //ManagementBaseObject outParams = classObj.InvokeMethod("Create", inParams, null);
+
+        }
 
     }
 }
+
+
+//https://www.softcom.cz/eshop/tp-link-tl-wn722n-bezdratovy-lite-n-usb-klient-802-11n-ext-antena_d85859.html
+
